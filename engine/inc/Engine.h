c08da7cae5e73f2d70cba74e48cc479e47c23211
Gunhee LEE <gunheecs.lee@samsung.com>
2020-11-02 15:15:56 +0900

    Added loadWork and saveWork function to Engine class.

--- engine/inc/Engine.h
+++ engine/inc/Engine.h
@@ -1,6 +1,8 @@
 #ifndef __ENGINE_H__
 #define __ENGINE_H__
 
+#include "Work.h"
+
 namespace FWA
 {
     class Engine
@@ -8,6 +10,9 @@ namespace FWA
         // This class represents an engine instance. Each FWA app has only one Engine instance.
         private:
         public:
+            // Use Boost::serialization library for save and load operation.
+            void saveWork(const Work& workToBeSaved, std::string whereTo);
+            Work loadWork(std::string uniqueWorkId, std::string fromWhere);
     };
 }
 #endif
--- engine/src/Engine.cpp
+++ engine/src/Engine.cpp
@@ -1,1 +1,16 @@
 #include "Engine.h"
+
+namespace FWA
+{
+    void 
+    Engine::saveWork(const Work& workToBeSaved, std::string whereTo)
+    {
+
+    }
+
+    Work
+    Engine::loadWork(std::string uniqueWorkId, std::string fromWhere)
+    {
+
+    }
+}
