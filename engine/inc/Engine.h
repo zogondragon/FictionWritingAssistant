e90c5709109d071b4823c7b9367ddb066364b996
Gunhee LEE <gunheecs.lee@samsung.com>
2020-11-02 13:02:24 +0900

    Made Engine class, which represents an Engine instance for each FWA app instance.

---
+++ engine/inc/Engine.h
@@ -0,0 +1,13 @@
+#ifndef __ENGINE_H__
+#define __ENGINE_H__
+
+namespace FWA
+{
+    class Engine
+    {
+        // This class represents an engine instance. Each FWA app has only one Engine instance.
+        private:
+        public:
+    };
+}
+#endif
